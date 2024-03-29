02a40c045194bf98ad6135170a261d5958717bfb
StefanKirilov <stefank5248@gmail.com>
2024-03-29 11:22:04 +0200

    second commit

--- Rest-api/client/src/app/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    appUrl: 'http://localhost:3030'
-};
-
-export const USER_KEY = '[user]';
--- Rest-api/client/src/app/types/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    _id: string;
-    username: string,
-    email: string,
-    likedHouses: string[],
-    // myHouses: string[],
-    // bookedHouses: string[],
-}
